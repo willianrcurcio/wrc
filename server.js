7ab886940d36b17e2226ff722d14c403d2412769
Willian RC <willianrezendecurcio@hotmail.com>
2021-05-01 11:55:59 +0000

    db connect

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+const db = mongoose.connection
+db.once('open', () => console.log('We are connected to database'))
+db.on('error', (error) => console.error(error))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
